ca9895b9ed15f62af52118166e6f29b2808a14d8
Antonio <antonio.lovotrico@icloud.com>
2020-11-12 16:23:26 +0100

    project completed

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+//chiedi all'utente il suo nome
+var name = prompt("Qual'è il tuo nome?");
+
+//chiedi all'utente il suo cognome
+var surname = prompt("Qual'è il tuo cognome?");
+
+//chiedi all'utente il suo colore preferito
+var favouriteColour = prompt("Qual'è il tuo colore preferito?");
+
+//Scrivi password ad hoc aggiungendo il numero 20 alla fine
+document.getElementById("insecure_pass").innerHTML = name + surname + favouriteColour + 20
+
+
